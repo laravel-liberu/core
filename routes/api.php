f8a92544ff63262da0bf1f76e02ab1fb636866d2
Curtis Delicata <curtis.delicata@outlook.com>
2023-10-19 07:52:36 +0100

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -6,9 +6,7 @@ use LaravelLiberu\Core\Http\Controllers\Guest;
 Route::prefix('api')
     ->group(function () {
         Route::get('/meta', Guest::class)->name('meta');
-
-        require __DIR__.'/app/auth.php';
-
+        
         Route::middleware(['api', 'auth', 'core'])
             ->group(fn () => require __DIR__.'/app/core.php');
     });
